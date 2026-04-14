5df2ed4931afc7866215c5ffd9ddfccde28d889d
Jessica <jmmwikali936@gmail.com>
2026-04-14 11:45:10 +0300

    updates

--- app/src/main/java/com/example/sokogarden/MainActivity.kt
+++ app/src/main/java/com/example/sokogarden/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.sokogarden
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -34,6 +36,32 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(applicationContext, Signin::class.java)
             startActivity(intent)
         }
+
+//        HOW TO RETRIEVE DATA FROM APP STORAGE
+//        Find the textview by id
+        val usernameText = findViewById<TextView>(R.id.usernameTextView)
+
+//        First access the file where the data is stored
+        val prefs = getSharedPreferences("user_session",Context.MODE_PRIVATE)
+
+//        Get the data by use of the key
+        val username = prefs.getString("username", "Guest")
+
+//        Update UI
+        usernameText.text = "Welcome $username"
+
+//        LOGOUT BUTTON - CLEARS DATA IN APP STORAGE
+//        val logoutbutton = findViewById<Button>(R.id.logoutbutton)
+//        Setting on click listener
+//        logoutbutton.setOnClickListener {
+//        Accessing the file that the data was stored in
+//            val prefs = getSharedPreferences("user_session",Context.MODE_PRIVATE)
+//        Editing the file, clearing the data, saving the changes
+//            prefs.edit().clear().apply()
+//        Updating the text view
+//            usernameText.text = "WELCOME"
+//        }
+
 
     }
 }
